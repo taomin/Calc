d77c7322c561ba34ef4733b4a6cfa373027f59d0
freespeech <mr.ms.days@gmail.com>
2014-09-05 23:10:03 -0700

    Initial Commit

---
+++ Calc/CALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CALAppDelegate.h
+//  Calc
+//
+//  Created by Taomin Chang on 9/5/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calc
+//
+//  Created by Taomin Chang on 9/5/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CALAppDelegate class]));
+    }
+}
